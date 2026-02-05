95684cbfcc6402e8c8a14c9625b877ade247545d
kanackle <kanak.somani02@myhunter.cuny.edu>
2026-02-05 09:22:15 -0500

    Revenue by month and year

--- queries.sql
+++ queries.sql
@@ -1,9 +1,25 @@
 USE northwind;
-select *
-from products
-order by UnitPrice desc
-limit 5;
+
+select OrderID, SUM(UnitPrice * Quantity) as Revenue
+from orderdetails
+group by OrderID;
+
+select orders.OrderID, year(orders.OrderDate) as year, month(orders.OrderDate) as month,
+sum(orderdetails.UnitPrice * orderdetails.Quantity * (1-orderdetails.Discount)) as Revenue
+from orders 
+join orderdetails 
+on orderdetails.OrderID = orders.OrderID
+group by orders.OrderID
+order by year, month;
+
+select year(orders.OrderDate) as year, month(orders.OrderDate) as month,
+sum(orderdetails.UnitPrice * orderdetails.Quantity * (1-orderdetails.Discount)) as Revenue
+from orders 
+join orderdetails 
+on orderdetails.OrderID = orders.OrderID
+group by year(orders.OrderDate), month(orders.OrderDate)
+order by year, month;
 
 
-select * from customers
-where country = 'USA';
+-- select OrderID, year(OrderDate) as Year, month(OrderDate) as Month
+-- from orders;
